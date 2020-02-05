2e516ef59abb0d99eb9619c82e9bc45df8265d44
David Coleman <46888854+S00189824@users.noreply.github.com>
2020-02-05 12:20:31 +0000

    Song Class Created

---
+++ OODProject/OODProject/Songs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODProject
+{
+    class Songs : IComparable
+    {
+        public string SongName { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistsName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+        public Songs(string songname,string albumname,string artistname,DateTime date)
+        {
+            SongName = songname;
+            AlbumName = albumname;
+            ArtistsName = artistname;
+            ReleaseDate = date;
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
