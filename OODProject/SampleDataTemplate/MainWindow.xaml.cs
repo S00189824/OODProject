8098b407e52cd3e78b5dee3293de99c432748be9
David Coleman <46888854+S00189824@users.noreply.github.com>
2020-02-25 13:07:04 +0000

    Update V1.12

--- OODProject/SampleDataTemplate/MainWindow.xaml.cs
+++ OODProject/SampleDataTemplate/MainWindow.xaml.cs
@@ -13,24 +13,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OODProject
+namespace SampleDataTemplate
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Artists> AllArtists = new List<Artists>();
-
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            
+            lbxMusic.ItemsSource;
         }
     }
 }
