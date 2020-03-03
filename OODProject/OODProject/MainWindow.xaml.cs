06074baa024ad9000894a2f4d445842105b48bc8
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-03 01:45:09 +0000

    Updated

--- OODProject/OODProject/MainWindow.xaml.cs
+++ OODProject/OODProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,8 @@ namespace OODProject
     public partial class MainWindow : Window
     {
         List<Songs> AllSongs = new List<Songs>();
+        MediaPlayer songsToPlay;
+        FileInfo SongsFile;
 
 
         public MainWindow()
@@ -31,14 +34,25 @@ namespace OODProject
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Songs S1 = new Songs("sdfhjsf", 1, new DateTime(2019,1,10),new TimeSpan(0,2,22));
-            Songs S2 = new Songs("aaa", 2, new DateTime(2015,09,10),new TimeSpan(0,2,55));
-            Songs S3 = new Songs("aaa", 3, new DateTime(2014,09,10),new TimeSpan(0,3,55));
-            Songs S4 = new Songs("aaa", 4, new DateTime(2020,09,10),new TimeSpan(0,4,55));
-            Songs S5 = new Songs("aaa", 5, new DateTime(2019,09,10),new TimeSpan(0,1,55));
-            Songs S6 = new Songs("aaa", 6, new DateTime(2016,09,10),new TimeSpan(0,4,05));
-            Songs S7 = new Songs("aaa", 7, new DateTime(2017,09,10),new TimeSpan(0,3,34));
-            Songs S8 = new Songs("aaa", 8, new DateTime(2015,09,10),new TimeSpan(0,3,59));
+            DisplaySongs();
+
+            
+
+        }
+
+
+        void DisplaySongs()
+        {
+            Songs S1 = new Songs("sdfhjsf", 1, new DateTime(2019, 1, 10), new TimeSpan(0, 2, 22));
+            Songs S2 = new Songs("aaa", 2, new DateTime(2015, 09, 10), new TimeSpan(0, 2, 55));
+            Songs S3 = new Songs("aaa", 3, new DateTime(2014, 09, 10), new TimeSpan(0, 3, 55));
+            Songs S4 = new Songs("aaa", 4, new DateTime(2020, 09, 10), new TimeSpan(0, 4, 55));
+            Songs S5 = new Songs("aaa", 5, new DateTime(2019, 09, 10), new TimeSpan(0, 1, 55));
+            Songs S6 = new Songs("aaa", 6, new DateTime(2016, 09, 10), new TimeSpan(0, 4, 05));
+            Songs S7 = new Songs("aaa", 7, new DateTime(2017, 09, 10), new TimeSpan(0, 3, 34));
+            Songs S8 = new Songs("aaa", 8, new DateTime(2015, 09, 10), new TimeSpan(0, 3, 59));
+
+
 
             AllSongs.Add(S1);
             AllSongs.Add(S2);
@@ -51,6 +65,35 @@ namespace OODProject
             AllSongs.Add(S8);
 
             lbxMusic.ItemsSource = AllSongs;
+        }
+
+
+        public void selectfolders(string filename)
+        {
+            FileInfo_Class fclass;
+            DirectoryInfo dirInfo = new DirectoryInfo(filename);
+
+            FileInfo[] info = dirInfo.GetFiles("*.*");
+            foreach (FileInfo f in info)
+            {
+                fclass = new FileInfo_Class();
+                fclass.Name = f.Name;
+                fclass.length = Convert.ToUInt32(f.Length);
+                fclass.DirectoryName = f.DirectoryName;
+                fclass.FullName = f.FullName;
+                fclass.Extension = f.Extension;
+                obcinfo.Add(fclass);
+            }
+            DirectoryInfo[] subDirectories = dirInfo.GetDirectories();
+            foreach (DirectoryInfo directory in subDirectories)
+            {
+                selectfolders(directory.FullName);
+            }
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            songsToPlay.Play();
         }
     }
 }
