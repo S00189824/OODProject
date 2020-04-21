771403728565d4eeec66daaf3bd13cfb0306b093
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-04-22 00:10:37 +0100

    nbg

--- OODProject/MusicManager/Program.cs
+++ OODProject/MusicManager/Program.cs
@@ -15,19 +15,24 @@ namespace MusicManager
 
             using (db)
             {
-                Artists artist1 = new Artists() { ID = 1, Artistname = "NierAutomata", Genre = "Composed" };
-                Songs song1 = new Songs() { SongID = 1, SongName = "War & War", SongFile = "Mp3", SongImage = "Pic2",ID = 1, Artists = artist1};
+                /*Artists artist1 = new Artists() { ID = 1, Artistname = "NierAutomata", Genre = "Composed" };
+                Songs song1 = new Songs() { SongID = 1, SongName = "War & War", SongFile = "Mp3", SongImage = "Pic2",ID = 1, Artists = artist1};*/
+                //Songs song3 = new Songs() { SongID = 3, SongName = "Dark Colossus - Kaiju",SongFile = "Mp3",SongImage = "Pic2",ID = 1,Artists =  }
 
-                Artists artist2 = new Artists() { ID = 2, Artistname = "Witcher OST", Genre = "Composed"};
-                Songs song2 = new Songs() { SongID = 2, SongName = "GWENT The Witcher Card Game OST - Monstrous Might", SongFile = "Mp3", SongImage = "Pic1",ID = 2,Artists = artist2};
+                /*Artists artist2 = new Artists() { ID = 2, Artistname = "Witcher OST", Genre = "Composed"};
+                Songs song2 = new Songs() { SongID = 2, SongName = "GWENT The Witcher Card Game OST - Monstrous Might", SongFile = "Mp3", SongImage = "Pic1",ID = 2,Artists = artist2};*/
 
-                db.Artists.Add(artist1);
+                /*db.Artists.Add(artist1);
                 db.Artists.Add(artist2);
 
                 Console.WriteLine("Added Artists to database");
 
                 db.Songs.Add(song1);
-                db.Songs.Add(song2);
+                db.Songs.Add(song2);*/
+
+                
+
+
 
                 Console.WriteLine("Added Songs to Database");
 
--- OODProject/OODProject/MainWindow.xaml.cs
+++ OODProject/OODProject/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace OODProject
         List<Artists> Artists = new List<Artists>();
         DirectoryInfo desktopfolder = new DirectoryInfo(@"C:\Users\david\OneDrive\Desktop" + "\\Music");
         SongData db = new SongData();
+        
 
 
         public MainWindow()
@@ -37,10 +38,10 @@ namespace OODProject
         {
             
 
-            var query = from s in db.Songs
-                        select s;
-            //var result = query.ToList();
-            lbxMusic.ItemsSource = query.ToList();
+            //var query = from a in db.Artists
+            //            select a;
+            ////var result = query.ToList();
+            //lbxMusic.ItemsSource = query.ToList();
             
         }
 
@@ -61,6 +62,34 @@ namespace OODProject
         }
 
         private void LbxMusic_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LbxMusic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string directoryname_Nier = @"C:\Users\David\Documents\GitHub\OODProject\OODProject\OODProject\Artists\Witcher\";
+            string directoryname_Witcher = @"C:\Users\David\Documents\GitHub\OODProject\OODProject\OODProject\Artists\Nier\";
+
+            string[] Nier_songs = { "Dark Colossus - Kaiju", "Emil - Despair", "Significance - Nothing - J'Nique Nicole", "The Sound of the End", "War & War" };
+
+            var query = from a in db.Artists
+                        select a;
+            //var result = query.ToList();
+            lbxMusic.ItemsSource = query.ToList();
+        }
+
+        private void BtnCycleLeft_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCycleRight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void SetArtist()
         {
 
         }
